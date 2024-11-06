d7cd727a2a3d6c7e2c7e5d2525222308161f1da1
Aaron <aaron@agilitycms.com>
2024-11-06 11:01:23 -0500

    Removing duplicate declarations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,2 @@
 
-export const environment = {
-  production: false,
-  AGILITY_GUID: '',
-  AGILITY_API_FETCH_KEY: '',
-  AGILITY_API_PREVIEW_KEY: '',
-  AGILITY_LOCALE: '',
-  AGILITY_SITEMAP: ''
-};
+// these will be written by your .env file
